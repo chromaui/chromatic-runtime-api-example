43228d1b01ffc7d499345a665d26ee783f3cbdf8
Tom Coleman <tom@thesnail.org>
2018-08-20 11:28:37 +1000

    Added `react-chromatic` and runtime API

--- src/DesignSystem.js
+++ src/DesignSystem.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import * as headerExamples from './examples/Header';
 
+import configure from 'react-chromatic';
+
+const runtime = {
+  runtime: 'design-system',
+  specs: () =>
+    Object.keys(headerExamples).map(name => ({
+      component: { name: 'Header' },
+      name,
+      input: '{}',
+    })),
+  renderSpec: ({ component: { name: componentName }, name }) => {
+    if (componentName !== 'Header') {
+      throw new Error('Unknown component!');
+    }
+
+    return headerExamples[name]();
+  },
+};
+
+configure(runtime);
+
 export default () => {
   return (
     <div>
